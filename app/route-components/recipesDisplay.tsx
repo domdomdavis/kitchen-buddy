5c7f9c8b4cc5afc9f36308750bdfaf9f04839950
Dom Davis <d0miniqu3davis@gmail.com>
2024-05-29 11:24:21 -0400

    resize photos in home page

--- app/route-components/recipesDisplay.tsx
+++ app/route-components/recipesDisplay.tsx
@@ -5,14 +5,14 @@ type RecipeDisplayProps = {
 };
 export const RecipesDisplay = ({ recipes }: RecipeDisplayProps) => {
   return recipes.map((recipe) => (
-    <div key={recipe.id} className="flex flex-col w-96 h-96">
+    <div key={recipe.id} className="flex flex-col">
       <Link to={`/recipes/${recipe.id}`}>
         <p className="font-semibold text-xl text-center" key={recipe.id}>
           {recipe.title}
         </p>
         <img
           src={recipe.photo_url}
-          className="object-scale-down border-4 border-violet-400 rounded-md"
+          className="object-cover border-4 border-violet-400 rounded-md h-80 w-80"
         />
       </Link>
     </div>
