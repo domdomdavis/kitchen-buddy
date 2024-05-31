994f8b590a21d8f182e478328f3ec6e7997dcb70
Dom Davis <d0miniqu3davis@gmail.com>
2024-05-31 15:35:29 -0400

    add to inventory, search inventory

--- app/routes/inventory.tsx
+++ app/routes/inventory.tsx
@@ -9,10 +9,19 @@ import { db } from "~/utils/db.server";
 export async function action({ request }: ActionFunctionArgs) {
   const prisma = new PrismaClient();
   const { formData } = await request.json();
-  const matchingRecipes = await prisma.recipe.findMany({
-    where: { id: { in: formData } },
-  });
-  return matchingRecipes;
+  if (!formData.addingItem) {
+    const matchingRecipes = await prisma.recipe.findMany({
+      where: { id: { in: formData.ids } },
+    });
+    return matchingRecipes;
+  } else {
+    const newItem = await prisma.inventory.create({
+      data: {
+        item: formData.item,
+      },
+    });
+    return newItem;
+  }
 }
 export const loader = async () => {
   const inventory = await db.inventory.findMany();
@@ -23,7 +32,10 @@ type LoaderType = Awaited<ReturnType<typeof loader>>;
 
 export default function Inventory() {
   const { inventory, ingredientList } = useLoaderData<LoaderType>();
+  const [itemList, setItemList] = useState(inventory);
   const [recipes, setRecipes] = useState<RecipeType[]>([]);
+  const [newItemInput, setNewItemInput] = useState("");
+  const [searchInput, setSearchInput] = useState("");
   const fetcher = useFetcher();
   const findRecipes = (item: string) => {
     const recipeIds: string[] = [];
@@ -33,33 +45,80 @@ export default function Inventory() {
     });
     fetcher.submit(
       {
-        formData: recipeIds,
+        formData: { ids: recipeIds, addingItem: false },
+      },
+      { method: "POST", action: "/inventory", encType: "application/json" }
+    );
+  };
+  const addItemToInventory = (item: string) => {
+    fetcher.submit(
+      {
+        formData: { item, addingItem: true },
       },
       { method: "POST", action: "/inventory", encType: "application/json" }
     );
   };
   useEffect(() => {
     if (fetcher.data) {
+      console.log(fetcher.data);
       setRecipes(fetcher.data as RecipeType[]);
     }
   }, [fetcher.data]);
 
+  const searchInventory = (searchInput: string) => {
+    const matching = inventory.filter((item) =>
+      item.item.includes(searchInput)
+    );
+    setItemList(matching);
+  };
   return (
     <div className="p-8">
-      <h1 className="text-center text-4xl font-medium">My Inventory</h1>
-      {inventory.map((item) => (
-        <button
-          className="m-4 p-4 rounded-md bg-pink-300 font-medium text-xl"
-          onClick={() => findRecipes(item.item)}
-        >
-          {item.item}
-        </button>
-      ))}
-      {recipes.length > 0 && (
-        <div className="flex space-x-4">
-          <RecipesDisplay recipes={recipes} />
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-medium">My Inventory</h1>
+        <div className="flex-row">
+          {" "}
+          <input
+            placeholder="Add Item"
+            className="m-4 p-4 border-2 border-orange-300 rounded-md"
+            value={newItemInput}
+            onChange={(e) => setNewItemInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.code === "Enter") {
+                addItemToInventory(newItemInput);
+                setNewItemInput("");
+              }
+            }}
+          />
+          <input
+            placeholder="Search inventory"
+            className="m-4 p-4 border-2 border-orange-300 rounded-md"
+            value={searchInput}
+            onChange={(e) => {
+              setSearchInput(e.target.value);
+              searchInventory(searchInput);
+              if (e.target.value === "") setItemList(inventory);
+            }}
+          />
         </div>
-      )}
+
+        <div className="flex-row">
+          {itemList.map((item) => (
+            <button
+              key={item.id}
+              className="m-4 p-4 rounded-md bg-pink-300 font-medium text-xl"
+              onClick={() => findRecipes(item.item)}
+            >
+              {item.item}
+            </button>
+          ))}
+        </div>
+
+        {recipes.length > 0 && (
+          <div className="flex space-x-4">
+            <RecipesDisplay recipes={recipes} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
